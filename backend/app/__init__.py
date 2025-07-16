b34cc386bd9138e4a0e2f57fb62dbe89370f7cd1
Robert Daniels <robdaniels2001@gmail.com>
2025-07-16 17:45:25 -0400

    implement fastapi app declarator

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 
+from celery import current_app as current_celery_app
+
+from app.core.config import settings
+
+def create_celery():
+    celery_app = current_celery_app
+    celery_app.config_from_object(settings, namespace='CELERY') # type: ignore
+
+    return celery_app
+
 def create_app() -> FastAPI:
     app = FastAPI()
+
+    app.celery_app = create_celery() # type: ignore
+
+    from features.log import log_router
+    app.include_router(log_router)
 
     @app.get('/')
     async def root():
@@ -9,4 +24,4 @@ def create_app() -> FastAPI:
     
     return app
 
-from . import ingestion
+from . import core, ingestion

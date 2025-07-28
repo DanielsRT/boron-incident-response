9e23915a3ef5ddfb75d97e031227afccc892364b
Robert Daniels <robdaniels2001@gmail.com>
2025-07-28 18:44:48 -0400

    Add log scripts

--- backend/app/log/service.py
+++ backend/app/log/service.py
@@ -2,13 +2,22 @@ import time
 import httpx
 import socket
 import json
+import logging
+import sys
 from azure.identity import ClientSecretCredential
-from fastapi import HTTPException
 from typing import Any, List, Dict
 
-from core.config import settings
-from log import log_router
+from app.core.config import settings
+from app.log import log_router
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s',
+    stream=sys.stdout
+)
+
+logger = logging.getLogger(__name__)
 
 _token_cache = {
     "access_token": None,
@@ -30,7 +39,7 @@ def get_access_token() -> str:
         client_id = settings.CLIENT_ID,
         client_secret = settings.CLIENT_SECRET
         )
-    token = credential.get_token("https://management.azure.com/.default")
+    token = credential.get_token("https://api.loganalytics.io/.default")
     _token_cache["access_token"] = token.token
     _token_cache["expires_at"] = token.expires_on - 60 # buffer
 
@@ -38,19 +47,27 @@ def get_access_token() -> str:
 
 def fetch_all_security_logs() -> List[Dict[str, Any]]:
     """Fetch all security events from Azure Log Analytics and returns a flat list of dicts"""
-    token = get_access_token()
-    url = f"https://api.loganalytics.io/v1/workspaces/{settings.WORKSPACE_ID}/query"
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
-    }
-    body = {"query": "SecurityEvents"}
+    logger.info("Getting access token...")
+    try:
+        token = get_access_token()
+        url = f"https://api.loganalytics.io/v1/workspaces/{settings.WORKSPACE_ID}/query"
+        headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
+        body = {"query": "SecurityEvent | where TimeGenerated >= ago(30d) | sort by TimeGenerated desc"}
 
-    resp = httpx.post(url, headers=headers, json=body, timeout=60)
-    resp.raise_for_status()
-    data = resp.json()
+    
+        resp = httpx.post(url, headers=headers, json=body, timeout=60)
+        resp.raise_for_status()
+        data = resp.json()
+        return flatten_response(data)
 
-    return flatten_response(data)
+    except httpx.HTTPError as e:
+        logger.error(f"Error fetching Azure logs: {e}")
+        raise
+
+    return []
 
 def flatten_response(response_json) -> List[Dict[str, Any]]:
     table = response_json.get("tables", [])
@@ -66,6 +83,7 @@ def flatten_response(response_json) -> List[Dict[str, Any]]:
     return all_logs
 
 def send_azure_logs_to_logstash(logs):
+    print("Sending Azure logs to logstash...")
     with socket.create_connection(("logstash", 5000)) as sock:
         for entry in logs:
             line = json.dumps(entry, default=str)+"\n"
